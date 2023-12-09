c081b6d910cde11dcf42e74edd6a1797a1202e0a
Dilip-Love-Code <118705027+dilip199016@users.noreply.github.com>
2023-12-09 16:32:55 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,19 @@
 // App.js
 import React from 'react';
-
 import MyComponent from '../components/MyComponent';
+import { UserProvider, ThemeProvider, LanguageProvider, ConfigProvider } from '../contexts';
 
 function App() {
   return (
+    <UserProvider>
+      <ThemeProvider>
+        <LanguageProvider>
+          <ConfigProvider>
             <MyComponent />
+          </ConfigProvider>
+        </LanguageProvider>
+      </ThemeProvider>
+    </UserProvider>
   );
 }
 
